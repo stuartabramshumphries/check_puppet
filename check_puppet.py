3c5533880485712874eff1c1de5fa68353cb0998
Stuart Abrams-Humphries <shumphries@jumptrading.com>
2014-06-06 15:44:11 +0100

    reg

--- check_puppet.py
+++ check_puppet.py
@@ -16,34 +16,29 @@ def check_ok():
     handler = logging.handlers.SysLogHandler(address='/dev/log')
     my_logger.addHandler(handler)
     disabledfile = "/home/shumphries/monitor-puppet/logfiles/agent_disabled.lock"
-    lrsummary = "/home/shumphries/monitor-puppet/logfiles/last_run_summary.yaml"
-    statefile = "/home/shumphries/monitor-puppet/logfiles/state.yaml"
+    lrsummary    = "/home/shumphries/monitor-puppet/logfiles/last_run_summary.yaml"
+    statefile    = "/home/shumphries/monitor-puppet/logfiles/state.yaml"
 
     if os.path.isfile(disabledfile):
         with open(disabledfile,'r') as dis:
             data = yaml.load(dis)
-        msg = gethostname() + "Puppet disabled: " + data["disabled_message"]
+        msg = gethostname() + " Puppet disabled: " + data["disabled_message"]
         my_logger.critical(msg)
 
-    if os.path.isfile(statefile):
+    if os.path.isfile(lrsummary):
+        with open(lrsummary, "r") as fp:
+            data = yaml.load(fp)
+        timeran = data["time"]["last_run"]
+        timediff = time.time() - timeran
+        if timediff > 3601:
+           timediff2 = str(datetime.timedelta(seconds=timediff)) 
+           msg = "Puppet not run on " + gethostname() + " for " + timediff2
+           my_logger.critical(msg)
+
+'''    if os.path.isfile(statefile):
         (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(statefile)
         lastrun = mtime
-
-        with open(lrsummary, "r") as fp:
-            for line in fp:
-                if 'last_run:' in line:
-                    line2 = line.strip()
-                    l1, l2 = line2.split(":", 1)
-                    l3 = int(l2)
-                    linef = int(l3)
-                    timediff = time.time() - linef
-                    if timediff > 3601:
-                        timediff2 = str(datetime.timedelta(seconds=timediff))
-                        msg = "Puppet not run on " + gethostname() + " for " + timediff2
-                        my_logger.critical(msg)
-
-    sys.exit()
-
+        '''
 
 if __name__ == '__main__':
     check_ok()
