cb7619c0e7876b126d56661e24046fdcf512e637
Stuart Abrams-Humphries <shumphries@jumptrading.com>
2014-08-20 16:06:19 +0100

    pep8 fixes

--- check_puppet.py
+++ check_puppet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import sys
 import time
 import yaml
 import os.path
@@ -11,9 +10,9 @@ from socket import gethostname
 
 
 disabledfile = "/home/shumphries/monitor-puppet/logfiles/agent_disabled.lock"
-lrsummary    = "/home/shumphries/monitor-puppet/logfiles/last_run_summary.yaml"
-statefile    = "/home/shumphries/monitor-puppet/logfiles/state.yaml"
-lrreport     = "/home/shumphries/monitor-puppet/logfiles/last_run_report.yaml"
+lrsummary = "/home/shumphries/monitor-puppet/logfiles/last_run_summary.yaml"
+statefile = "/home/shumphries/monitor-puppet/logfiles/state.yaml"
+lrreport = "/home/shumphries/monitor-puppet/logfiles/last_run_report.yaml"
 
 #    agentLockfile = "/var/lib/puppet/state/agent_catalog_run.lock"
 #    disablefile = "/var/lib/puppet/state/agent_disabled.lock"
@@ -26,41 +25,54 @@ my_logger.setLevel(logging.DEBUG)
 handler = logging.handlers.SysLogHandler(address='/dev/log')
 my_logger.addHandler(handler)
 
-def check_last_time_run():
-    ''' disabledfile = "/var/lib/puppet/state/agent_disabled.lock"
-    lrsummary    = "/var/lib/puppet/state/last_run_summary.yaml"
-    statefile    = "/var/lib/puppet/state/state.yaml" '''
 
+def construct_ruby_object(loader, suffix, node):
+    return loader.construct_yaml_map(node)
+
+
+def construct_ruby_sym(loader, node):
+    return loader.construct_yaml_str(node)
+
+
+yaml.add_multi_constructor(u"!ruby/object:", construct_ruby_object)
+yaml.add_constructor(u"!ruby/sym", construct_ruby_sym)
+
+
+def check_last_time_run():
+    # disabledfile = "/var/lib/puppet/state/agent_disabled.lock"
+    # lrsummary    = "/var/lib/puppet/state/last_run_summary.yaml"
+    # statefile    = "/var/lib/puppet/state/state.yaml"
     if os.path.isfile(lrsummary):
         with open(lrsummary, "r") as fp:
             data = yaml.load(fp)
         timeran = data["time"]["last_run"]
         timediff = time.time() - timeran
         if timediff > 3601:
-           timediff2 = str(datetime.timedelta(seconds=timediff)) 
-           msg = gethostname() + ": Puppet not run for" + timediff2
-           print msg
-#          my_logger.critical(msg)
+            timediff2 = str(datetime.timedelta(seconds=timediff))
+            msg = gethostname() + ": Puppet not run for" + timediff2
+            print msg
+#           my_logger.critical(msg)
 
 
 def check_if_disabled():
     if os.path.isfile(disabledfile):
-        with open(disabledfile,'r') as dis:
+        with open(disabledfile, 'r') as dis:
             data = yaml.load(dis)
-        msg = gethostname() + ": Puppet disabled, reason: " + data["disabled_message"]
+        msg = gethostname()+": Puppet disabled,: "+data["disabled_message"]
         print msg
 #        my_logger.critical(msg)
+
 
 def check_if_failures():
     print "in check failures"
     if os.path.isfile(lrreport):
-        print "found file",lrreport
-        with open(lrreport,'r') as lrr:
+        print "found file", lrreport
+        with open(lrreport, 'r') as lrr:
             data = yaml.load(lrr)
-        exit()
-'''        if data["events"]["status"] == "failure":
-            print data["events"]["message"]
-'''        
+        for key in data:
+            print key
+        # if data["resource_statuses"]["events"]["status"] == "failure":
+        # print data["resource_statuses"]["events"]["message"]
 
 if __name__ == '__main__':
     check_if_failures()
